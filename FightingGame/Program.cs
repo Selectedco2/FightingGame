457d4d361a9a072b0647283053ff56d204ff9ab1
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2023-09-21 10:56:37 +0200

    Skrev hur Slump and While loopar funkar

--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿// SLUMP:
+
+// Random generator = new Random();
+
+// int slumptal = 10 + generator.Next(10, 20);
+
+// Console.WriteLine(slumptal);
+
+
+// WHILE LOOP
+
+// int tal = 100;
+
+// while (tal > 0)
+// {
+//     Console.WriteLine(tal);
+//     tal++; eller tal--;
+    
+// }
+// Console.WriteLine("Färdig!");
+
+int YourHealth = 100;
+int EnemyHealth = 100;
+
+
+
+
+
+
+
+Console.ReadLine();
